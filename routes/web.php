f504b4793ad6796df83a1d9468cbadea390de88b
Zumayaa <abzu_21@alu.uabcs.mx>
2024-11-19 11:25:25 -0700

    rutas creadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/users', action: [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', action: [UserController::class, 'create'])->name('users.create');
+Route::get('/users/{id}', action: [UserController::class, 'show'])->name('users.show');
+Route::get('/users/update/{id}', action: [UserController::class, 'edit'])->name('users.edit');
 
 Route::get('/', function () {
     return view('welcome');
